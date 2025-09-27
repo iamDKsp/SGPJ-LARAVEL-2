3482da77de8d20639a7ded857cd9c580013b4909
IhateDKsp <silvatarcisio2201@gmail.com>
2025-09-27 13:59:31 -0300

    Restore Laravel 10 bootstrap configuration

--- LARAVEL/nome-do-projeto/bootstrap/app.php
+++ LARAVEL/nome-do-projeto/bootstrap/app.php
@@ -1,20 +1,30 @@
 <?php
 
+use App\Console\Kernel as AppConsoleKernel;
+use App\Exceptions\Handler as AppExceptionHandler;
+use App\Http\Kernel as AppHttpKernel;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })
-    ->create();
+$app = new Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
+
+$app->singleton(
+    ExceptionHandler::class,
+    AppExceptionHandler::class
+);
+
+$app->singleton(
+    ConsoleKernel::class,
+    AppConsoleKernel::class
+);
+
+$app->singleton(
+    HttpKernel::class,
+    AppHttpKernel::class
+);
+
+return $app;
