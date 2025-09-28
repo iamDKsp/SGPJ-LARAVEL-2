12c79f3f78b21b6bf3f658545319477fdf034ea8
IhateDKsp <silvatarcisio2201@gmail.com>
2025-09-28 16:51:39 -0300

    Remove placeholder lockfile and document dependency install

---
+++ LARAVEL/nome-do-projeto/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
